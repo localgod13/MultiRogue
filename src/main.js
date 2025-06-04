b9bfe2848c70b265b78e679414e4d0c08ccf52b4
Toxic <localgod13@gmail.com>
2025-06-03 18:55:03 -0700

    network3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 import { Game } from './game.js';
 import { NetworkManager } from './network.js';
 
